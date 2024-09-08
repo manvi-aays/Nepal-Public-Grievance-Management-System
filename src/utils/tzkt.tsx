fed3995c61eec46d9af0653d5cb131806d6157c2
manvi-aays <manvitiwari@aaysinsight.com>
2024-09-09 01:26:14 +0530

    Update tzkt.tsx

--- src/utils/tzkt.tsx
+++ src/utils/tzkt.tsx
@@ -3,6 +3,7 @@ import { TezosToolkit } from '@taquito/taquito'; // Correct import
 // Initialize Tezos toolkit with mainnet RPC URL
 const tezos = new TezosToolkit('https://api.tez.ie/rpc/mainnet/');
 
+// Async function to interact with the Tezos blockchain
 const tzKTConverter = async (address: string): Promise<void> => {
   try {
     // Fetch balance for the given address

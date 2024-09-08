ace7cb4038cc0f08d2bbe8820ccff789a5004ecd
manvi-aays <manvitiwari@aaysinsight.com>
2024-09-09 01:34:24 +0530

    Update tzkt.tsx

--- src/utils/tzkt.tsx
+++ src/utils/tzkt.tsx
@@ -3,7 +3,7 @@ import { TezosToolkit } from '@taquito/taquito'; // Ensure you have the correct 
 const endpoint = new TezosToolkit('https://api.tez.ie/rpc/mainnet/');
 
 // Correct async function definition
-const tzKTConverter = async (address: string): Promise<void> => {
+// const tzKTConverter = async (address: string): Promise<void> => {
   // Function body should be filled with actual logic
   // For now, it is just a placeholder
-};
+// };

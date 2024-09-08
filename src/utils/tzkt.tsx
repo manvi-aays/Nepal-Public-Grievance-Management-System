3f6ce0c9792a388f2f7026387fb681c8cb9ed29e
Shreyas Singh <94439098+4shreyas5@users.noreply.github.com>
2024-09-08 23:12:27 +0530

    Update tzkt.tsx

--- src/utils/tzkt.tsx
+++ src/utils/tzkt.tsx
@@ -1,5 +1,12 @@
-{
+// Corrected Code
+import { TezosToolkit } from '@taquito/taquito'; // Ensure you have the correct import
+
+const endpoint = new TezosToolkit('https://api.tez.ie/rpc/mainnet/');
+const tzKTConverter = async (address: string) => {
+ {
   "compilerOptions": {
     "isolatedModules": false
   }
 }
+};
+
